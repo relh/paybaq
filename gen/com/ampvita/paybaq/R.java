400ada7dd478a2e8f9cb651d8d786d8fef5fac9f
Wajahat Siddiqui <agentwaj@gmail.com>
2013-09-22 05:06:41 -0400

    Change colors.

--- gen/com/ampvita/paybaq/R.java
+++ gen/com/ampvita/paybaq/R.java
@@ -41,8 +41,9 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int paybaq_logo=0x7f020001;
+        public static final int custom_button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int paybaq_logo=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0012;
@@ -105,11 +106,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080002;
+        public static final int AppBaseTheme=0x7f080003;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080003;
+        public static final int AppTheme=0x7f080004;
+        public static final int customButton=0x7f080002;
         public static final int edittext=0x7f080000;
         public static final int textview=0x7f080001;
     }
